acfe2a81c43165b94fc12cff15344e715b5682db
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-08-10 05:27:29 +0530

    implemented useRef with hide/show inputbox

---
+++ src/useComponentVisible.tsx
@@ -0,0 +1,29 @@
+import { useState, useEffect, useRef } from 'react';
+export default function useComponentVisible(initialIsVisible: boolean) {
+  const [isComponentVisible, setIsComponentVisible] =
+    useState(initialIsVisible);
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleHideDropdown = (event: KeyboardEvent) => {
+    if (event.key === 'Escape') {
+      setIsComponentVisible(false);
+    }
+  };
+
+  const handleClickOutside = (event: any) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsComponentVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('keydown', handleHideDropdown, true);
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('keydown', handleHideDropdown, true);
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  });
+
+  return { ref, isComponentVisible, setIsComponentVisible };
+}
